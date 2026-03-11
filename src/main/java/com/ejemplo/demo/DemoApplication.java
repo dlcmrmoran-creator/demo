a4594d85e6f2a43c0ef0531c93e2237ba72350c8
dlcmrmoran-creator <dlcmrmoran@gmail.com>
2026-03-11 16:45:12 -0600

    revision (#8)
    Co-authored-by: EMANUELMED <emedrano@iwa.com.mx>

--- src/main/java/com/ejemplo/demo/DemoApplication.java
+++ src/main/java/com/ejemplo/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.ejemplo.demo;
+package com.ejemplo.demo;//agrega comentario
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
